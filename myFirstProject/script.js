ee549a658a2df19f3b76600468c51cf3fb9aa0f7
NohaAlsaeed <142249163+NohaAlsaeed@users.noreply.github.com>
2023-08-23 17:51:50 +0300

    Add files via upload
    About me.

---
+++ myFirstProject/script.js
@@ -0,0 +1,21 @@
+function funEduc(){
+    document.getElementById("edu").innerHTML="Bachelor in Information Systems";
+}
+
+function funskill(){
+    document.getElementById('skill').innerHTML="HTML, CSS, JavaScript, Python"
+}
+
+function funAccou(){
+    document.getElementById('accou').innerHTML='1- CoderHub &#9754;';
+    document.getElementById('accou').style.textDecoration= 'none';
+    document.getElementById('accou1').innerHTML=`<br>2- soloLearn &#9754;<br><br>`;
+    document.getElementById('accou1').style.textDecoration= 'none';
+}
+
+
+function funcontact(){
+    document.getElementById('con').innerHTML='<p>Email: Noha1alsaeed@gmail.com</p>';
+    
+}
+
